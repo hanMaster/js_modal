1df122a254c6b0ee6ba2d872c5e7819a4b638275
hanMaster <w54661c@gmail.com>
2020-02-21 11:22:20 +1000

    Done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 window.onload = () => {
   const modal = $.modal({
     title: 'hello modal',
-    content: 'bla bla',
-    width: '200px',
-    closable: true
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt eius enim explicabo fugit porro praesentium quibusdam quidem rem soluta?',
+    width: '400px',
+    closable: true,
   });
   const openBtn = document.querySelector('#showModal');
-  openBtn.addEventListener('click', ()=>{modal.open()});
+  const setContentBtn = document.querySelector('#setContent');
+  openBtn.addEventListener('click', () => {
+    modal.open();
+  });
+  setContentBtn.addEventListener('click', () => {
+    modal.setContent(`<p>Current time is ${new Date().toLocaleTimeString()}</p>`);
+  });
 };
-
--- js/plugins/modal.js
+++ js/plugins/modal.js
@@ -1,8 +1,7 @@
 function _createModal(options) {
-
   let html = `
     <div id="mo" class="modal-overlay">
-      <div class="modal-window" style="width: ${options.width}">
+      <div id="mw" class="modal-window" style="width: ${options.width}">
         <div class="modal-header">
             <span class="modal-title">
                 ${options.title}
@@ -12,14 +11,13 @@ function _createModal(options) {
     html += `<span id="x" class="modal-close">&times;</span>`;
   }
 
-  html +=
-    `</div>
-        <div class="modal-body">
+  html += `</div>
+        <div id="mb" class="modal-body">
             ${options.content}
         </div>
         <div class="modal-footer">
-            <button class="btn btn-primary">Ok</button>
-            <button class="btn btn-secondary">Cancel</buttonclass>
+            <button class="btn btn-primary animated infinite heartBeat delay-2s">Ok</button>
+            <button class="btn btn-secondary" id="cancel">Cancel</buttonclass>
         </div>
       </div>
     </div>
@@ -30,34 +28,32 @@ function _createModal(options) {
   modal.insertAdjacentHTML('afterbegin', html);
   document.body.appendChild(modal);
 
-  return modal
-
+  return modal;
 }
 
-
 /*
-*  title:string
-* closable: boolean
-* content: string
-* width: string
-* destroy(): void | remove listeners | cut from DOM
-* close window on cross and blur
-* ---------------
-* setContent(html: string): void | PUBLIC
-* onClose(): void called after close window
-* onOpen(): void called after open window
-* beforeClose(): boolean | true => allow close | false => reject close
-* ----------
-* animate.css
-* */
-$.modal = function (options) {
-
+ *  title:string
+ * closable: boolean
+ * content: string
+ * width: string
+ * destroy(): void | remove listeners | cut from DOM
+ * close window on cross and blur
+ * ---------------
+ * setContent(html: string): void | PUBLIC
+ * onClose(): void called after close window
+ * onOpen(): void called after open window
+ * beforeClose(): boolean | true => allow close | false => reject close
+ * ----------
+ * animate.css
+ * */
+$.modal = function(options) {
   const ANIMATION_SPEED = 200;
   let closing = false;
 
   const modal$ = _createModal(options);
 
   const fnClose = () => {
+    if (!beforeClose()) return;
     closing = true;
     modal$.classList.remove('open');
     modal$.classList.add('hiding');
@@ -65,24 +61,57 @@ $.modal = function (options) {
       modal$.classList.remove('hiding');
       closing = false;
     }, ANIMATION_SPEED);
+    onClose();
   };
 
-  document.querySelector('#x')
-    .addEventListener('click', fnClose);
+  const fnBlur = e => {
+    const outElement = document.getElementById('mw');
+    let targetElement = e.target;
 
-  document.querySelector('#mo')
-    .addEventListener('click', fnClose);
+    do {
+      if (targetElement === outElement) {
+        return;
+      }
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    fnClose();
+  };
+
+  const onOpen = () => {
+    console.log('Open event handler');
+  };
+
+  const beforeClose = () => {
+    const permit = Math.random() > 0.5;
+    console.log('Permit', permit);
+    return permit;
+  };
+
+  const onClose = () => {
+    console.log('Close event handler');
+  };
+
+  document.querySelector('#x').addEventListener('click', fnClose);
+  document.querySelector('#cancel').addEventListener('click', fnClose);
+
+  document.querySelector('#mo').addEventListener('click', e => fnBlur(e));
 
   return {
     open() {
-      !closing && modal$.classList.add('open')
+      !closing && modal$.classList.add('open');
+      onOpen();
     },
     close() {
-      fnClose()
+      fnClose();
+    },
+    setContent(content) {
+      const body = document.getElementById('mb');
+      body.innerHTML = content;
     },
     destroy() {
-      document.querySelector('#mo').removeEventListener('click',fnClose);
+      document.querySelector('#mo').removeEventListener('click', fnClose);
       document.querySelector('#x').removeEventListener('click', fnClose);
-    }
-  }
+    },
+  };
 };
